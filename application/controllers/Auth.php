5e78f532cfb3ee01696ce91a74ed164fea6c1cbf
Yoel Hezron Paulitua Simarmata <56376874+yoelhezron@users.noreply.github.com>
2021-06-09 23:30:22 +0700

    Menambahkan file registernyaaa

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -82,4 +82,147 @@ class Auth extends CI_Controller
 
 	}
 
+		public function registration()
+	{
+		if($this->session->userdata('email')) {
+			redirect('user');
+		}
+
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+			'is_unique' => 'This email has already registered!'
+		]);
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+			'matches' => 'Password Dont Match!',
+			'min_length' => 'Password Too Short!'
+		]);
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+		if( $this->form_validation->run() == false) {
+			$data['title'] = 'Registration';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('templates/auth_footer');
+		} else {
+			$email = $this->input->post('email', true);
+			$data = [
+				'name' => htmlspecialchars($this->input->post('name', true)),
+				'email' => htmlspecialchars($email),
+				'image' => 'default.jpg',
+				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 0,
+				'date_created' => time()
+			];
+
+			//siapkan token
+			$token = base64_encode(random_bytes(32));
+			$user_token = [
+				'email' => $email,
+				'token' => $token,
+				'date_created' => time()
+			];
+
+			$this->db->insert('user', $data);
+			$this->db->insert('user_token', $user_token);
+
+			//kirim email
+			$this->_sendEmail($token, 'verify');
+
+			
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratuliation! Your account has been created. Please check your email to activate your account!</div>');
+			redirect('auth/login');
+
+		}
+	}
+
+	private function _sendEmail($token, $type)
+	{
+		$config = [
+			'protocol' => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_user' => 'aplikasipinjamhelp@gmail.com',
+			'smtp_pass' => 'jessicaer15',
+			'smtp_port' => 465,
+			'mailtype' => 'html',
+			'charset' => 'utf-8',
+			'newline' => "\r\n"
+		];
+
+		$this->load->library('email');
+		$this->email->initialize($config);
+
+		$this->email->from('aplikasipinjamhelp@gmail.com', 'Pinjam Admin');
+		$this->email->to($this->input->post('email'));
+
+		if($type == 'verify') {
+			$this->email->subject('Account Verification');
+			$this->email->message('Click this link to verify your account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '& token=' . urlencode($token) . '">Active</a>');
+		} else if($type == 'forgot') {
+			$this->email->subject('Reset Password');
+			$this->email->message('Click this link to reset your Password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '& token=' . urlencode($token) . '">Reset Password</a>'); 
+		}
+		
+
+		if($this->email->send()) {
+			return true;
+		} else {
+			echo $this->email->print_debugger();
+			die; 
+		}
+	}
+
+	public function verify()
+	{
+		$email = $this->input->get('email');
+		$token = $this->input->get('token');
+
+		$user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+		//mengecek email yg diurl benar atau tidak
+		if($user) {
+			$user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+			//mengecek token ada didatabase 
+			if($user_token) {
+
+				//untuk mengecek waktu batasan pengaktifan token
+				if(time() - $user_token['date_created'] < (60*60*24)) {
+
+					//update data ke database
+					$this->db->set('is_active', 1);
+					$this->db->where('email', $email);
+					$this->db->update('user');
+
+					//delete token
+					$this->db->delete('user_token', ['email' => $email]);
+
+					$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">'. $email .' has been activated. Please login.</div>');
+						redirect('auth/login');
+
+				} else {
+
+					//menghapus user dari database karna token udah habis 
+					$this->db->delete('user', ['email' => $email]);
+					$this->db->delete('user_token', ['email' => $email]);
+
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Token expired.</div>');
+						redirect('auth/login');
+
+				}
+
+
+			} else {
+				
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Token Invalid</div>');
+					redirect('auth/login');
+			}
+		} else {
+
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong Email.</div>');
+				redirect('auth/login');		
+		}
+	}
+
+	
 }
