d3e7562a1c8640c2f6358a4fddeb5fcdd99652d6
Jessica Elizabeth R <jessicaaer00@gmail.com>
2021-06-09 23:39:41 +0700

    Menambahkan fungsi barang

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -97,4 +97,155 @@ class Admin extends CI_Controller
 		$this->db->delete('user_role');
 		redirect('admin/role');
 	}
+
+    public function barang() 
+	{
+		$data['title'] = 'Data Barang';
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+		//$data['barang'] = $this->db->get('tbl_barang')->result_array();
+
+		//pagination
+		$this->load->library('pagination');
+
+		//ambil data keyword
+		if($this->input->post('submit')) {
+			$data['keyword'] = $this->input->post('keyword');
+			$this->session->set_userdata('keyword' , $data['keyword']);
+		} else {
+			$data['keyword'] =$this->session->userdata('keyword');
+		}
+
+		//config
+		$this->db->like('name' , $data['keyword']);
+		$this->db->or_like('desc' , $data['keyword']);
+		$this->db->from('tbl_barang');
+		$config['total_rows'] = $this->db->count_all_results();
+		$data['total_rows'] = $config['total_rows'];
+		$config['per_page']=5;
+		
+		//initialize
+		$this->pagination->initialize($config);
+
+		$data['start'] = $this->uri->segment(3);
+		$data['barang'] = $this->Model_barang->getBarang($config['per_page'],$data['start'], $data['keyword']);
+
+		$this->form_validation->set_rules('name', 'Nama Barang', 'required');
+		$this->form_validation->set_rules('desc', 'Deskripsi Barang', 'required');
+		$this->form_validation->set_rules('stock', 'Stock Barang', 'required');
+		
+		if ($this->form_validation->run() == false ) {
+			$this->load->view('templates/header', $data);
+			$this->load->view('templates/sidebar', $data);
+			$this->load->view('templates/topbar', $data);
+			$this->load->view('admin/barang', $data);
+			$this->load->view('templates/footer');
+		} else {
+			//Tambah Data
+			$name	= $this->input->post('name');
+			$desc	= $this->input->post('desc');
+			$stock	= $this->input->post('stock');
+			$gambar	= $_FILES['gambar']['name'];
+			
+			if($gambar) 
+			{
+				$config ['upload_path'] = './assets/img/profile';
+				$config ['allowed_types'] = 'jpg|jpeg|png|gif';
+				
+				$this->load->library('upload', $config);
+				
+				if(!$this->upload->do_upload('gambar'))
+				{
+					echo "Gambar Gagal Diupload!";
+				}
+				
+				else
+				{
+					$gambar = $this->upload->data('file_name');
+				}
+				
+				$data = array(
+					'name'		=> $name,
+					'desc'		=> $desc,
+					'stock'		=> $stock,
+					'gambar'	=> $gambar			
+				);
+
+				$this->Model_barang->tambah_barang($data, 'tbl_barang');
+				$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Barang ditambah!</div>');
+				redirect('admin/barang');
+			}
+		}
+		
+	}
+
+	public function editBarang($id_barang)
+	{
+		$data['title'] = 'Edit Barang'; 
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+		$where = array('id_barang' => $id_barang);
+		$data['barang'] = $this->Model_barang->edit_barang($where, 'tbl_barang')->result();
+
+		$this->form_validation->set_rules('name', 'Nama Barang', 'required|trim');
+		$this->form_validation->set_rules('desc', 'Kondisi Barang', 'required|trim');
+		$this->form_validation->set_rules('stock', 'Stock Barang', 'required|trim|numeric');
+
+		if($this->form_validation->run() == false) {
+			$this->load->view('templates/header', $data);
+			$this->load->view('templates/sidebar', $data);
+			$this->load->view('templates/topbar', $data);
+			$this->load->view('admin/barang-edit', $data);
+			$this->load->view('templates/footer');
+		} else {
+			$id_barang 	= $this->input->post('id_barang');
+			$name		= $this->input->post('name');
+			$desc		= $this->input->post('desc');
+			$stock		= $this->input->post('stock');
+			$gambar		= $_FILES['gambar']['name'];
+
+			//jika ada upload gambar
+			if(!empty($gambar)) 
+			{
+				$config ['upload_path'] = './assets/img/profile';
+				$config ['allowed_types'] = 'jpg|jpeg|png|gif';
+
+				$this->load->library('upload', $config);
+				
+				if( $this->upload->do_upload('gambar')) 
+				{
+					$gambar = $this->upload->data("file_name");
+				}
+			}
+			else 
+			{
+				$gambar = $this->input->post('old_image');
+			}
+
+			$data = array(
+
+				'name'		=> $name,
+				'desc'		=> $desc,
+				'stock'		=> $stock, 
+				'gambar'	=> $gambar,
+			);
+
+			$where = array(
+				'id_barang' => $id_barang
+			);
+
+			$this->Model_barang->update_data($where, $data, 'tbl_barang');
+			
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Barang berhasil diedit!</div>');
+			redirect('admin/barang');
+		}
+		
+	}
+	
+	public function deleteBarang($id_barang)
+	{
+		$this->db->where('id_barang', $id_barang);
+		$this->db->delete('tbl_barang');
+		redirect('admin/barang');
+	}
 }
